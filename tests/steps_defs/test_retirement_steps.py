126af39271df5d6b12303cd8bc9f53d1909f1066
maserratesulzer <70975596+maserratesulzer@users.noreply.github.com>
2020-11-30 16:20:10 -0500

    Update test_retirement_steps.py

--- tests/steps_defs/test_retirement_steps.py
+++ tests/steps_defs/test_retirement_steps.py
@@ -5,7 +5,7 @@ EXTRA_TYPES = {'Number': int, 'String': str}
 CONVERTERS = {'birth_year': int, 'years_old': int, 'months_old': int}
 
 
-@scenario("retirement.feature", "user will input a valid birth month", example_converters=CONVERTERS, features_base_dir="/home/travis/build/maserratesulzer/Retirement_test_bdd/tests/features/retirement.feature")
+@scenario("retirement.feature", "user will input a valid birth month", example_converters=CONVERTERS, features_base_dir="/home/travis/build/maserratesulzer/Retirement_test_bdd/tests/features")
 def test_add():
     pass
 
