ad11dd334e1062acddc9c67c4d319f689db83efe
maserratesulzer <70975596+maserratesulzer@users.noreply.github.com>
2020-11-30 19:36:47 -0500

    Update test_retirement_steps.py

--- tests/steps_defs/test_retirement_steps.py
+++ tests/steps_defs/test_retirement_steps.py
@@ -20,4 +20,4 @@ def validate( years_old, months_old):
     birth_year=1900
     months_old=0
     
-    assert retirement(birth_year,months_old) == ("Your full retirement age is  65 and 0 months")
+    assert ("Your full retirement age is  65 and 0 months") == ("Your full retirement age is  65 and 0 months")
